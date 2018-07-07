fafafbdc48060b5a871a922f8c69971e921084e9
EooHoSun <redysj0517@naver.com>
2018-07-07 23:48:39 +0900

    #2 올렸다 이 세끼야

---
+++ eclipse-workspace/Practice/src/Pr01_Backjoon/Ex01_ForEx.java
@@ -0,0 +1,60 @@
+package Pr01_Backjoon;
+
+import java.util.*;
+
+public class Ex01_ForEx {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+//		int N = input.nextInt();
+		
+//		for(int i =1; i<=N; i++) {
+//			for(int j = 0; j <i; j++) {
+//				System.out.print("*");	
+//			}
+//			System.out.println();
+//		}
+		
+//		for(int i =0; i<N; i++) {
+//			
+//			for(int j = N-1; j>i; j--) {
+//				System.out.printf(" ");
+//			}
+//			for(int j = 0; j<=i; j++) {
+//				System.out.printf("*");
+//			}
+//			System.out.println();
+//		}
+		
+//		for(int i =0; i<N; i++) {
+//			
+//			for(int j = N; j>i; j--) {
+//				System.out.printf("*");
+//			}
+//			System.out.println();
+//		}
+		
+//		for(int i =0; i<N; i++) {
+//			for(int j = 1; j<=i;j++) {
+//				System.out.printf(" ");
+//			}
+//			for(int j = N; j>i; j--) {
+//				System.out.printf("*");				
+//			}
+//			System.out.println();
+//		}
+		
+		int x = input.nextInt();
+		int y = input.nextInt();
+		int day;
+		
+		
+		
+		
+	
+		
+	
+	
+}
+
+}
